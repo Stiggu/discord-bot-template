476cf23ae631a6065697c21c2185666864707779
Stiggu <abelmunda@hotmail.com>
2021-11-15 01:25:55 -0400

    Added parser for dices in messages, added hints to functions

--- cogs/message_listener.py
+++ cogs/message_listener.py
@@ -1,5 +1,6 @@
+import discord.errors
 from discord.ext import commands
-from cogs.message_parser import parse_message
+from cogs.message_parser import parse_message, roll_dice
 
 
 class Listener(commands.Cog):
@@ -15,15 +16,27 @@ class Listener(commands.Cog):
         """
         Listen to every message that is sent on the server
         :param message: Contains Data (Check the API reference, this message has certain limitations)
-        :return:
         """
-        parse_message(message.content)
+
+        if ":dr" in message.content:
+            number_of_dices, sides = parse_message(message.content)
+
+            try:
+                number_of_dices = int(number_of_dices)
+                sides = int(sides)
+            except ValueError:
+                return await message.channel.send("Invalid query!")
+
+            rolls = roll_dice(number_of_dices, sides)
+            try:
+                await message.channel.send(rolls)
+            except discord.errors.HTTPException:
+                await message.channel.send("You used to many dices, are you crazy?")
 
 
 def setup(client):
     """
     Called by the main file, sets up the cog to be used
     :param client:
-    :return:
     """
     client.add_cog(Listener(client))
--- cogs/message_parser.py
+++ cogs/message_parser.py
@@ -1,8 +1,27 @@
-def parse_message(message):
+import random
+
+
+def roll_dice(number: int, sides: int) -> list:
+    """
+    Rolls a x amount of dices with y number of sides
+    :param number: int
+    :param sides: int
+    :return: int array with the rolls
+    """
+
+    return [random.randint(0, sides) for x in range(number)]
+
+
+def parse_message(message: str) -> list:
     """
     Will parse the message into something useful
-    :param message: Message from an user
+    :param str message: Message from an user
     :return: Returns an array with parsed data
     """
-    print(message)
-    return message
+
+    message_array_with_dice_data = message.split(':')[:2]
+
+    dices = "".join(filter(str.isdigit, message_array_with_dice_data[0]))
+    sides = "".join(filter(str.isdigit, message_array_with_dice_data[1]))
+
+    return [dices, sides]
